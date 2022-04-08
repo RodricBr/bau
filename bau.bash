e839abfa4cc57faa4592585dad28dde7ba08fa5c
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-04-08 20:12:38 -0300

    Update bau.bash

--- bau.bash
+++ bau.bash
@@ -1,10 +1,5 @@
 #!/usr/bin/bash
 
-# any spaces in the parameters will become separators between new parameters
-# Qualquer espaço no parametro vai se tornar separadores
-# ex: "php sql txt"  --> "php,sql,txt"
-set -- $*
-
 ULTI="${@: -1}"
 SUBSS="${*: -2:1}" # Do lado do ultimo
 
@@ -13,13 +8,14 @@ if [[ -z "$1" ]]; then
 #https://index.commoncrawl.org/CC-MAIN-2022-05-index?url=$1/*&output=json" | jq '.url'
 fi
 
+# Primeira etapa sem crawling de subdominios
+# First phase without subdomain crawling
 if [[ "$2" == "-ns" ]] && [[ ! -z "$1" ]]; then
   curl -s -k --tcp-fastopen --tcp-nodelay --connect-timeout 25 "https://web.archive.org/cdx/search/cdx?url=$1/*&output=json&fl=original&collapse=urlkey&page=/" | tr -d '[',']','"' | egrep ".*?:\/\/.*\?.*\=[^$]" | egrep -v ".($ULTI)" | uniq
   exit 0
-fi
-
-# Segunda etapa com anaálise de subdominios
-if [[ "$2" == "-s" ]] && [[ ! -z "$1" ]]; then
+# Segunda etapa com crawling de subdominios
+# Second phase with subdomain crawling
+elif [[ "$2" == "-s" ]] && [[ ! -z "$1" ]]; then
   curl -s -k --tcp-fastopen --tcp-nodelay --connect-timeout 25 "https://web.archive.org/cdx/search/cdx?url=*.$1/*&output=json&fl=original&collapse=urlkey&page=/" | tr -d '[',']','"' | egrep ".*?:\/\/.*\?.*\=[^$]" | egrep -v ".($ULTI)" | uniq
   exit 0
 fi
